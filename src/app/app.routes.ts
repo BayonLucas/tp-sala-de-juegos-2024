3b7a61e17dd6dda4694a6481fd3267c1c022dae3
BayonLucas <lucas.bayon31@hotmail.com>
2024-04-23 18:12:51 -0300

    Ruteo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,10 @@ import { ResetPasswordComponent } from './components/reset-password/reset-passwo
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'singup', component: SingupComponent },
     { path: 'reset-password', component: ResetPasswordComponent },
-    { path: 'home', component: HomeComponent },
     { path: '**', component: ErrorFoundComponent },
 ];
